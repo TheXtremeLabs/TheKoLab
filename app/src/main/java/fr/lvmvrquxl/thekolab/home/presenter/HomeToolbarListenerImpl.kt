728a6a29c7628537b68172fc0c5d99535b6ee09b
Loïc Lamarque <loiclamarque777@gmail.com>
2021-01-24 00:09:34 +0100

    feat: [#1] :sparkles: Update collapsing toolbar listener on offset changed

--- app/src/main/java/fr/lvmvrquxl/thekolab/home/presenter/HomeToolbarListenerImpl.kt
+++ app/src/main/java/fr/lvmvrquxl/thekolab/home/presenter/HomeToolbarListenerImpl.kt
@@ -6,14 +6,15 @@ import fr.lvmvrquxl.thekolab.home.view.HomeView
 class HomeToolbarListenerImpl(private val view: HomeView) : HomeToolbarListener {
     companion object {
         private const val INITIAL_SCROLL_RANGE: Int = -1
-        private const val OFFSET_LIMIT: Int = 0
+        private const val OFFSET_MAX: Int = 589
+        private const val OFFSET_LIMIT: Int = (OFFSET_MAX * 5) / 100
     }
 
     private var scrollRange: Int = INITIAL_SCROLL_RANGE
 
     override fun onOffsetChanged(appBar: AppBarLayout, verticalOffset: Int) {
         if (INITIAL_SCROLL_RANGE == this.scrollRange) this.initScrollRange(appBar)
-        if (OFFSET_LIMIT == this.scrollRange + verticalOffset)
+        if (OFFSET_LIMIT >= this.scrollRange + verticalOffset)
             this.view.showCollapsingToolbarTitle()
         else this.view.hideCollapsingToolbarTitle()
     }
