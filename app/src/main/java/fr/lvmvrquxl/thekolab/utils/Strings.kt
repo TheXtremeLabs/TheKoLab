1fa5d69785380102e9e1d16f6103427ef50aa8e6
Loïc Lamarque <loiclamarque777@gmail.com>
2021-01-25 21:47:00 +0100

    docs: [#1] :memo: Add documentation in string utilities

--- app/src/main/java/fr/lvmvrquxl/thekolab/utils/Strings.kt
+++ app/src/main/java/fr/lvmvrquxl/thekolab/utils/Strings.kt
@@ -3,8 +3,25 @@ package fr.lvmvrquxl.thekolab.utils
 import android.content.Context
 import fr.lvmvrquxl.thekolab.R
 
+/**
+ * String utilities.
+ *
+ * This class is responsible for accessing all the string resources of the application.
+ *
+ * @since 0.1.3
+ */
 object Strings {
     private const val APP_NAME: Int = R.string.app_name
 
+    /**
+     * Get the application's name.
+     *
+     * This method retrieves the application's name from given context (ex : an activity).
+     *
+     * @param context The context for retrieving the application's
+     * @return The application's name
+     *
+     * @since 0.1.3
+     */
     fun appName(context: Context): String = context.getString(APP_NAME)
 }
