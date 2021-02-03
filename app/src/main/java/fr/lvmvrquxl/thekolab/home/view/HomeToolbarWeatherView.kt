65a3203dd2c8063f5b375b9860686c1e5db84867
Loïc Lamarque <loiclamarque777@gmail.com>
2021-02-03 13:47:58 +0100

    style: [#2] :lipstick: Add fading animation on weather

--- app/src/main/java/fr/lvmvrquxl/thekolab/home/view/HomeToolbarWeatherView.kt
+++ app/src/main/java/fr/lvmvrquxl/thekolab/home/view/HomeToolbarWeatherView.kt
@@ -1,9 +1,12 @@
 package fr.lvmvrquxl.thekolab.home.view
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
 import android.app.Activity
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ProgressBar
 import fr.lvmvrquxl.thekolab.base.BaseView
 import fr.lvmvrquxl.thekolab.databinding.HomeToolbarWeatherFragmentBinding
 import fr.lvmvrquxl.thekolab.home.presenter.HomeToolbarWeatherPresenter
@@ -17,6 +20,7 @@ internal class HomeToolbarWeatherView(
         get() = this.fragment.activity
 
     private var presenter: HomeToolbarWeatherPresenter? = null
+    private var shortAnimationDuration: Int = 0
 
     init {
         this.bindViews()
@@ -41,9 +45,27 @@ internal class HomeToolbarWeatherView(
         super.viewBinding?.weatherLocationCountry?.apply { this.text = country }
     }
 
-    internal fun showWeatherInfo() {
-        super.viewBinding?.weatherInfo?.visibility = View.VISIBLE
-        super.viewBinding?.weatherProgressBar?.visibility = View.GONE
+    internal fun showWeatherInfo() = this.activity?.let { activity: Activity ->
+        this.shortAnimationDuration =
+            activity.resources.getInteger(android.R.integer.config_mediumAnimTime)
+        super.viewBinding?.weatherInfo?.apply {
+            this.alpha = 0f
+            this.visibility = View.VISIBLE
+            this.animate()
+                .alpha(1f)
+                .setDuration(shortAnimationDuration.toLong())
+                .setListener(null)
+        }
+        super.viewBinding?.weatherProgressBar?.let { progressBar: ProgressBar ->
+            progressBar.animate()
+                .alpha(0f)
+                .setDuration(shortAnimationDuration.toLong())
+                .setListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator) {
+                        progressBar.visibility = View.GONE
+                    }
+                })
+        }
     }
 
     private fun bindViews() {
