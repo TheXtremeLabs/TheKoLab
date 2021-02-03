f0e1c183e52e6b786e937985feeda541a8181d80
Loïc Lamarque <loiclamarque777@gmail.com>
2021-02-03 22:30:49 +0100

    docs: [#2] :memo: Add documentation in main activity

--- app/src/main/java/fr/lvmvrquxl/thekolab/MainActivity.kt
+++ app/src/main/java/fr/lvmvrquxl/thekolab/MainActivity.kt
@@ -4,6 +4,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import fr.lvmvrquxl.thekolab.home.view.HomeActivity
 
+/**
+ * Main activity.
+ *
+ * This class is the entry point of the application.
+ * It is responsible for loading the first view, which is currently [HomeActivity].
+ *
+ * @since 0.1.3
+ */
 internal class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
