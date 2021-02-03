f0ecfae6faee33a67ba2ad2e1bf065d5eb776f1a
Loïc Lamarque <loiclamarque777@gmail.com>
2021-02-03 22:21:58 +0100

    refactor: [#2] :recycle: Refactor app module

--- app/src/main/java/fr/lvmvrquxl/thekolab/MainActivity.kt
+++ app/src/main/java/fr/lvmvrquxl/thekolab/MainActivity.kt
@@ -8,9 +8,11 @@ import fr.lvmvrquxl.thekolab.home.view.HomeActivity
 internal class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        this.startActivity(
-            Intent(this, HomeActivity::class.java)
-                .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
-        )
+        this.goToHome()
     }
+
+    private fun goToHome() = this.startActivity(
+        Intent(this, HomeActivity::class.java)
+            .addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
+    )
 }
