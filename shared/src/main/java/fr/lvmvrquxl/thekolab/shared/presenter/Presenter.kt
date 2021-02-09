e8f4e53414de95d379912c94ea161dd0fd80deee
Loïc Lamarque <loiclamarque777@gmail.com>
2021-02-09 18:55:22 +0100

    docs: [#2] :memo: Add base presenter documentation

--- shared/src/main/java/fr/lvmvrquxl/thekolab/shared/presenter/Presenter.kt
+++ shared/src/main/java/fr/lvmvrquxl/thekolab/shared/presenter/Presenter.kt
@@ -4,18 +4,38 @@ import androidx.annotation.CallSuper
 import fr.lvmvrquxl.thekolab.core.tracker.Tracker
 import kotlinx.coroutines.*
 
+/**
+ * Parent of all presenters.
+ *
+ * @since 0.1.3
+ */
 abstract class Presenter {
     companion object {
         private const val SCOPE_NAME: String = "Presenter"
     }
 
+    /**
+     * Scope of the coroutines.
+     *
+     * @since 0.1.3
+     *
+     * @see [CoroutineName]
+     * @see [CoroutineScope]
+     */
     protected val coroutineScope: CoroutineScope = CoroutineScope(CoroutineName(SCOPE_NAME))
+
+    /**
+     * Trackers to use.
+     *
+     * @since 0.1.3
+     *
+     * @see [List]
+     * @see [Tracker]
+     */
     protected var trackers: List<Tracker> = listOf()
 
     /**
      * Cancel all coroutines in the presenter's scope.
-     *
-     * This method cancels all running coroutines in the presenter's scope.
      *
      * @since 0.1.3
      */
